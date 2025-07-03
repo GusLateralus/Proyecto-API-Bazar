d1c9474fe7eb0845418eace2e49020097e1de380
Gus <guslateralus@gmail.com>
2025-07-03 18:46:13 -0500

    Primer avance del proyecto

---
+++ bazar/src/main/java/com/proyectofinal/bazar/model/Producto.java
@@ -0,0 +1,35 @@
+
+package com.proyectofinal.bazar.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long codigo_producto;
+    private String nombre;
+    private double costo;
+    private double cantidad_disponible;
+    
+    public Producto()
+    {
+        
+    }
+
+    public Producto(Long codigo_producto, String nombre, double costo, double cantidad_disponible) {
+        this.codigo_producto = codigo_producto;
+        this.nombre = nombre;
+        this.costo = costo;
+        this.cantidad_disponible = cantidad_disponible;
+    }
+    
+    
+    
+}
